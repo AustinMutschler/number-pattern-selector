4fe6f8a50bdf11d65db0217d9bfa2661b62ea935
Austin Mutschler <austinmutschler@gmail.com>
2023-11-28 14:07:24 -0700

    removed type declarations for now (#5)

--- dist/parser/numberPatternParser.d.ts
+++
@@ -1,7 +0,0 @@
-import { PatternParserOptions } from "../index.d.js";
-export interface InternalPatternParserOptions {
-    min: number;
-    max: number;
-    rangeSelector: string;
-}
-export default function numberPatternParser(patternString: string, options?: PatternParserOptions): number[];
